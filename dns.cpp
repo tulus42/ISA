609c249cb93866982c9eabcf943ead7feb1e1f7a
arijec42 <adis.tulusak@gmai.com>
2019-11-17 21:14:23 +0100

    Documentation

--- dns.cpp
+++ dns.cpp
@@ -723,7 +723,7 @@ DNSType getType(unsigned short tmpType) {
         return(MR);
 
     case 0x0a:
-        std::cout << "NULL, ";
+        std::cout << "NULLdns, ";
         return(NULLdns);
 
     case 0x0b:
